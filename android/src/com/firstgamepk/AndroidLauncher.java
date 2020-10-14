2df7e0bc588739e8a026ebb8a1a1eaf4690eb347
Kirill <benberov09@mail.ru>
2020-10-14 23:48:51 +0300

    Initial commit

---
+++ android/src/com/firstgamepk/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.firstgamepk;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.firstgamepk.FirstGamePK;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FirstGamePK(), config);
+	}
+}
